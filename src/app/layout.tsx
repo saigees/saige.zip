41dcf85ac76e86d6c273821cd3903c9ee00e7cd7
saigees <saigeeees@gmail.com>
2023-06-18 21:32:36 +0100

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,9 @@
 "use client";
 
-import { Metadata } from "next";
 import { Toaster } from "sonner";
 import Modal from "~/components/Modal";
 import Navbar from "~/components/Navbar";
 import "~/styles/globals.css";
-
-export const metadata: Metadata = {
-  title: "Saige",
-  themeColor: "#9333ea",
-  openGraph: {
-    url: "http://saige.zip",
-    description:
-      "A designer and developer based out of the uk, creating interfaces and innovative softwares.",
-    title: "Saige",
-    images: ["http://saige.zip/favicon.png"],
-  },
-};
 
 export default function RootLayout({
   children,
@@ -25,13 +12,21 @@ export default function RootLayout({
 }) {
   return (
     <body className="mx-auto max-w-7xl px-[2rem] selection:bg-purple-600 selection:bg-opacity-30">
+      <head>
+        <title>Saige</title>
+        <meta name="color-scheme" content="#9333ea" />
+        <meta property="og:url" content="https://saige.zip" />
+        <meta property="og:description" content="A designer and developer based out of the uk, creating interfaces and innovative softwares." />
+        <meta property="og:site_name" content="Saige" />
+        <meta property="og:image" content="http://saige.zip/favicon.png" />
+      </head>
       <Toaster
         duration={3000}
         visibleToasts={1}
         position="bottom-center"
         expand={false}
       />
-      <Modal/>
+      <Modal />
       <Navbar />
       <div className="mt-10">{children}</div>
     </body>
